9455845fe078558427be13ff982ffe649f079b6a
Gaurav Singh <gaurav07singh10@gmail.com>
2024-08-24 19:01:33 +0530

    footer and vetting process addeed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import Hero from '../../components/Hero/HeroSection'
+import Hero from '../../components/Hero/HeroSection';
+import Footer from '../../components/Footer/Footer';
+import VettingProcess from '../../components/VettingProcess/VettingProcess';
 
 const Home = () => {
   return (
     <>
       <Hero/>
+      <VettingProcess/>
+      <Footer/>
     </>
   )
 }
