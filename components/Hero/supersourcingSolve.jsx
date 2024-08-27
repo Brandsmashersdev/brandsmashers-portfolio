a69cfd23cd54b8dcdca43215d39f9621f1d2264d
Brandsmashersdev <brandsmashersdev@gmail.com>
2024-08-27 12:46:23 +0530

    Modify supersourcing solve Section

--- components/Hero/supersourcingSolve.jsx
+++ components/Hero/supersourcingSolve.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import styles from '@/styles/supersourcingSolve.module.css';
 
-
-const SuperSourcingSolve = ()=>{
-return(  
-<div className={styles.SupersolveContainer}>
-<div className={styles.SupersolveBox}>
-<h2>The problem that job platforms cannot solve, Supersourcing solves</h2>
-<p>No pre and post hiring hustle required, we take care of everything. Want to learn more?</p>
-<button className={styles.superSolvebutton}>Schedule a call</button>
-</div>
-</div>
-);
+const SuperSourcingSolve = () => {
+  return (
+    <div className={styles.SupersolveContainer}>
+      <div className={styles.SupersolveBox}>
+        <h2>The problem that job platforms cannot solve, Supersourcing solves</h2>
+        <p>No pre and post hiring hustle required, we take care of everything. Want to learn more?</p>
+        <button className={styles.superSolvebutton}>Schedule a call</button>
+      </div>
+    </div>
+  );
 };
+
 export default SuperSourcingSolve;
