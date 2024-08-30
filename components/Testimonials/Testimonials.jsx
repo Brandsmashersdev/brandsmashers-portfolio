7aa8fe14cd75b7ccbadaba881bbf0bcb85c67475
Brandsmashersdev <brandsmashersdev@gmail.com>
2024-08-30 12:11:42 +0530

    Add TextSlider at bootom, vetting process content designing, Testimonial card slider

--- components/Testimonials/Testimonials.jsx
+++ components/Testimonials/Testimonials.jsx
@@ -36,12 +36,15 @@ const Testimonials = () => {
         const settings = {
             dots: false,
             infinite: true,
-            speed: 500,
-            slidesToShow: 4,
+            speed: 1500,
+            slidesToShow: 3,
             slidesToScroll: 1,
             centerMode: false, 
-            nextArrow: <NextArrow />,
-            prevArrow: <PrevArrow />,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            cssEase: 'linear',
+            // nextArrow: <NextArrow />,
+            // prevArrow: <PrevArrow />,
             responsive: [
             {
                 breakpoint: 1024,
@@ -49,7 +52,8 @@ const Testimonials = () => {
                 slidesToShow: 2,
                 slidesToScroll: 1,
                 infinite: true,
-                dots: false
+                dots: false,
+                initialSlide:3
                 }
             },
             {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,6 +14,7 @@ import Testimonials from "../../components/Testimonials/Testimonials";
 import Investors from "../../components/Investors/Investors";
 import Offer from "../../components/Offer/Offer"
 import CaseStudies from '../../components/CaseStudies/caseStudies';
+import TextSlider from '../../components/TextSlider/TextSlider';
 
 const Home = () => {
   return (
@@ -31,6 +32,7 @@ const Home = () => {
       <FeatureSection />
       <FaqSection/>
       <Footer/>
+      <TextSlider/>
     </>
   )
 }
