041f1b498897c7a3dc47aa592184f13e4ba8f8aa
Gaurav Singh <gaurav07singh10@gmail.com>
2024-09-05 11:32:57 +0530

    about page changes

--- components/AboutUs/AboutHeader.jsx
+++ components/AboutUs/AboutHeader.jsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 const AboutHeader = () => {
   return (
     <div className={styles.aboutHeader}>
-     <Image src="/AboutUs/road-marking.jpg" alt="About Us Background" className={styles.backgroundImage} height={30} width={100}/>
+     <Image src="/AboutUs/AboutUs.jpg" alt="About Us Background" className={styles.backgroundImage} layout="fill" objectFit='cover'/>
       <div className={styles.overlay}>
         <h1>ABOUT US</h1>
         <p>Creating technology solutions that enable businesses to achieve success.</p>
