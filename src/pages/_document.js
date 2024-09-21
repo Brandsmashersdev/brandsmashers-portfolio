fd993dee51977407562b4bc217074ffa43d4a3f7
Raj Yadav <raj.yadav@tatvasoft.com>
2024-09-21 16:27:44 +0530

    meta data update

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,6 +3,22 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+      <title>Brandsmashers Technologies</title>
+      <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+      <meta
+        name="keywords"
+        content="portfolio, react nextjs portfolio, software company, brandsmashers"
+      />
+      <meta
+        name="description"
+        content="Brandsmashers Technologies - A software Company providing better software solutions"
+      />
+      <meta name="ibthemes" content="ATFN" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+      />
+      <link rel="icon" href="favicon.ico" />
       <Head />
       <body>
         <Main />
